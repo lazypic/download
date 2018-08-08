a9b24db9cac0685a909073247bfab6a5952e8287
kim han woong <khw7096@gmail.com>
2018-08-09 01:23:35 +0900

    key하나만 되어도 download시 문제가 없다.

--- download.go
+++ download.go
@@ -11,30 +11,16 @@ import (
 func main() {
 	regionPtr := flag.String("region", "ap-northeast-2", "AWS region name.")
 	bucketPtr := flag.String("bucket", "lazypic", "S3 bucket name.")
-	projectPtr := flag.String("project", "", "project name")
-	episodePtr := flag.Int("ep", 0, "episode number")
-	scenePtr := flag.Int("s", 0, "scene number")
-	cutPtr := flag.Int("c", 0, "cut number")
+	keyPtr := flag.String("key", "", "key name")
 	flag.Parse()
-	if *projectPtr == "" {
+	if *keyPtr == "" {
 		flag.PrintDefaults()
 		return
 	}
-	key := *projectPtr
-	if *episodePtr != 0 && *scenePtr == 0 && *cutPtr == 0 {
-		key = fmt.Sprintf("%s/%d", key, *episodePtr)
-	}
-	if *episodePtr != 0 && *scenePtr != 0 && *cutPtr == 0 {
-		key = fmt.Sprintf("%s/%d/%d", key, *episodePtr, *scenePtr)
-	}
-	if *episodePtr != 0 && *scenePtr != 0 && *cutPtr != 0 {
-		key = fmt.Sprintf("%s/%d/%d/%d", key, *episodePtr, *scenePtr, *cutPtr)
-	}
-
 	svc := s3.New(session.New(), &aws.Config{Region: aws.String(*regionPtr)})
 	params := &s3.ListObjectsInput{
 		Bucket: aws.String(*bucketPtr),
-		Prefix: aws.String(key),
+		Prefix: aws.String(*keyPtr),
 	}
 	resp, _ := svc.ListObjects(params)
 	for _, key := range resp.Contents {
