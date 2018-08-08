82f951873a6d1c4ca98dff8e1b006b21c2662c3d
kim han woong <khw7096@gmail.com>
2018-08-08 18:05:11 +0900

    add flag

--- download.go
+++ download.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -8,10 +9,32 @@ import (
 )
 
 func main() {
-	svc := s3.New(session.New(), &aws.Config{Region: aws.String("ap-northeast-2")})
+	regionPtr := flag.String("region", "ap-northeast-2", "AWS region name.")
+	bucketPtr := flag.String("bucket", "lazypic", "S3 bucket name.")
+	projectPtr := flag.String("project", "", "project name")
+	episodePtr := flag.Int("ep", 0, "episode number")
+	scenePtr := flag.Int("s", 0, "scene number")
+	cutPtr := flag.Int("c", 0, "cut number")
+	flag.Parse()
+	if *projectPtr == "" {
+		flag.PrintDefaults()
+		return
+	}
+	key := *projectPtr
+	if *episodePtr != 0 && *scenePtr == 0 && *cutPtr == 0 {
+		key = fmt.Sprintf("%s/%d", key, *episodePtr)
+	}
+	if *episodePtr != 0 && *scenePtr != 0 && *cutPtr == 0 {
+		key = fmt.Sprintf("%s/%d/%d", key, *episodePtr, *scenePtr)
+	}
+	if *episodePtr != 0 && *scenePtr != 0 && *cutPtr != 0 {
+		key = fmt.Sprintf("%s/%d/%d/%d", key, *episodePtr, *scenePtr, *cutPtr)
+	}
+
+	svc := s3.New(session.New(), &aws.Config{Region: aws.String(*regionPtr)})
 	params := &s3.ListObjectsInput{
-		Bucket: aws.String("lazypic"),
-		Prefix: aws.String("mamma/"),
+		Bucket: aws.String(*bucketPtr),
+		Prefix: aws.String(key),
 	}
 	resp, _ := svc.ListObjects(params)
 	for _, key := range resp.Contents {
